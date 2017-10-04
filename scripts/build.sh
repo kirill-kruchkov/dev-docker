97b280319c0200053df1536149432717774a4158
Kirill Kryuchkov <kirill.kruchkov@gmail.com>
2017-10-04 22:00:22 +0100

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 if [ -z "$(docker image ls | grep ${IMAGE_NAME})" ] ; then
 	(
-        cp -Rf dotfiles ./dockerfiles/${IMAGE_NAME} \
-        && docker build -t $IMAGE_NAME -f ./dockerfiles/${IMAGE_NAME}/Dockerfile ./ \
-        && rm -rf ./dockerfiles/${IMAGE_NAME}/dotfiles
+#        cp -Rf dotfiles ./dockerfiles/${IMAGE_NAME} \
+        docker build -t $IMAGE_NAME -f ./dockerfiles/${IMAGE_NAME}/Dockerfile . \
+#        && rm -rf ./dockerfiles/${IMAGE_NAME}/dotfiles
     )
 fi
